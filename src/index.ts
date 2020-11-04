45aff78aa44490ccbdd8536d3e5012972fb6b97d
C0C41N <0ct41n@gmail.com>
2020-11-04 16:54:14 +0500

    Express TS Boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import * as express from 'express';
-import { Request, Response } from 'express';
+import { Application, Request, Response } from 'express';
 
-const app = express();
-const { 
-  PORT = 3000
-} = process.env;
+import express = require('express');
+
+const app: Application = express();
+const { PORT = 3000 } = process.env;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send({
-    message: 'hello world!',
-  })
+	res.send({
+		message: 'hello world!',
+	});
 });
 
 if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log('server started at http://localhost:'+PORT);
-  });
+	app.listen(PORT, () => {
+		console.log('server started at http://localhost:' + PORT);
+	});
 }
 
 export default app;
